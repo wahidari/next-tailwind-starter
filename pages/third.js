214a503729a1b7ad0b393de218a856d893980713
Wahid Ari <wahiid.ari@gmail.com>
2021-08-16 21:36:54 +0700

    update

--- pages/third.js
+++ pages/third.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Footer from "../components/Footer"
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 
 export default function Second() {
     return (
